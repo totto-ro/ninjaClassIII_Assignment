150a379a608f05b65dfffc09102b54b3391d70e5
Sofía Esquivel <sofia.esquivel@lcieducation.net>
2021-11-17 22:23:35 -0600

    Completed

---
+++ ninjaClassIII.js
@@ -0,0 +1,97 @@
+class Ninja{
+    constructor( name, health, _speed, _strength ){
+        this.name = name;
+        this.health = 100;
+        var speed = 3;
+        var strength = 3;
+
+        this.showStats = function(){
+            console.log(`Name: ${this.name}, Health: ${this.health}, Speed:  ${speed}, Strength:  ${strength}` );
+        }
+
+        this.kick = function( ninja ) {
+            if( ninja instanceof Ninja){
+                let injury = strength * 15;
+                ninja.health -= injury;
+                console.log(`${ninja.name} was kicked by ${this.name} and lost ${injury} in Health!`);
+            }
+            else{
+                console.log( `${ninja.name} is not a ninja!!!` );
+            }
+        }
+    }
+
+    sayName(){
+        console.log( `My ninja name is ${this.name}!` );
+    } 
+
+
+    drinkSake(){
+        this.health += 10;
+    }
+
+    punch( ninja ){
+        if( ninja instanceof Ninja ){
+            ninja.health -= 5;
+            console.log( `${ninja.name} was punched by ${this.name} and lost 5 in Health!` );
+        }
+        else{
+            console.log( `${ninja.name} is not a ninja!!!` );
+        }
+    }
+
+}
+
+let ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+// -> "My ninja name is Hyabusa!"
+ninja1.showStats();
+// -> "Name: Hayabusa, Health: 100, Speed: 3, Strength: 3"
+ninja1.drinkSake();
+ninja1.showStats();
+
+let blueNinja = new Ninja("Goemon");
+let redNinja = new Ninja("Bill Gates");
+redNinja.punch(blueNinja);
+blueNinja.showStats();
+
+blueNinja.kick(redNinja);
+// -> "Bill Gates was kicked by Goemon and lost 15 Health!"
+// In this case, redNinja Bill Gates lost 15 health because blueNinja Goemon has 1 point of strength
+redNinja.showStats();
+
+console.log("----------------------------------------------------------------------------------------------");
+
+
+class Sensei extends Ninja{
+    constructor( name, _health, _speed, _strength, _wisdom ){
+        super( name );
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    showStats(){
+        console.log(`Name: ${this.name}, Health: ${this.health}, Speed:  ${this.speed}, Strength:  ${this.strength}, Wisdom: ${this.wisdom}` );
+    }
+
+    saySenseiName(){
+        super.sayName();
+    }
+
+    drinkSake(){
+        this.health += 10;
+    }
+
+    speakWisdom() {
+        super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+
+}
+
+let sensei = new Sensei("Master Splinter");
+	sensei.showStats();
+    sensei.saySenseiName();
+    sensei.speakWisdom();
